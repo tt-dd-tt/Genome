acd05dab6cc7a1d248ea0934e794e68ca0188091
ttsstt <libor.riska@gmail.com>
2023-01-18 15:16:49 +0200

    dask

--- biopython.py
+++ biopython.py
@@ -1,8 +1,21 @@
-import gzip
-from Bio import SeqIO
+import dask.dataframe as dd
+from dask.distributed import Client
+from sklearn.cluster import KMeans
 
+# Start a Dask cluster
+client = Client()
 
-with gzip.open("/Users/libor/Desktop/aaa.fastq.gz", "rt") as handle:
-...     for record in SeqIO.parse(handle, "fastq"):
-...         print(record)
-... 
+# Read the FASTQ file using Dask
+df = dd.read_csv("/path/to/large_file.fastq", sep='\n', header=None)
+
+# Convert the sequence data to a numpy array
+sequences = df[1].values.compute()
+
+# Initialize the k-means model with the desired number of clusters
+kmeans = KMeans(n_clusters=5)
+
+# Fit the model to the data
+kmeans.fit(sequences)
+
+# Print the cluster labels for each sequence
+print(kmeans.labels_)
